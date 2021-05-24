f6b0292d68dbc202124cd4adc33f1a9e9c126317
Jai Kumar <jaikumarofficial@gmail.com>
2021-05-23 23:32:45 -0300

    header added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
 import { Component } from "react";
 import Navbar from "./components/Navbar"
-import "./App.css"
+import Header from './components/Header'
+import Particles from "react-particles-js"
+
 
 class App extends Component {
     render() {
         return (
             <div>
+                <Particles 
+                    params={{
+                        particles: {
+                            number: {
+                                value: 40,
+                                density: {
+                                    enable: true,
+                                    value_area: 900
+                                },
+                                
+                            },
+                            // shape: {
+                            //   type: 'circle',
+                            //   stroke:{
+                            //       width: 5,
+                            //       color: "whitesmoke"
+                            //   }  
+                            // }
+                        }
+                    }}
+                />
                 <Navbar />    
-                <h1>resume</h1>
+                <Header />
+                <div><p>test</p></div>
             </div>
         )            
     }
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Typed from "react-typed"
+
+const Header = () => {
+    return (
+        <div className='header-wrapper'>
+            <div className='main-info'>
+                <h1>jai kumar</h1>
+                <Typed 
+                    className='typed-text'
+                    strings={['Full Stack Developer', 'Python', 'React', 'C++', 'Node JS',]}
+                    typeSpeed = {40}
+                    backSpeed = {40}
+                    loop
+                    
+                />
+                <a href='#' className='btn-main-offer'>CONTACT</a>
+            </div>
+        </div> 
+    )
+}
+
+export default Header
