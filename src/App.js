a4988cf332dff170bb8e32086eef50aa365e6efa
Jai Kumar <jaikumarofficial@gmail.com>
2021-05-23 21:14:55 -0300

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Component } from "react";
+import Navbar from "./components/Navbar"
+import "./App.css"
 
 class App extends Component {
     render() {
         return (
-            <h1>resume</h1>
+            <div>
+                <Navbar />    
+                <h1>resume</h1>
+            </div>
         )            
-        
     }
 }
 
